9786e48635d5770dea1178c28dfa5728b0674c66
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-13 21:19:58 +0530

    init

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust this if you're using a different framework
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
